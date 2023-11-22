24d4852a0e9af873bf3a2cd3cc3ea211adba0220
ArturoEden02 <arturo.aragonsanchez@hotmail.com>
2023-11-22 17:59:19 -0600

    mvvmlight libs

---
+++ Lands/Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,38 @@
+﻿namespace Lands.ViewModels
+{
+    using System;
+    using System.Windows.Input;
+    using GalaSoft.MvvmLight.Command;
+
+    public class LoginViewModel
+    {
+        #region Properties
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool isRunning { get; set; }
+        public bool isRemembered { get; set; }
+        #endregion
+
+        #region Constructor
+        public LoginViewModel()
+        {
+            this.isRemembered = true;
+        }
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+        private void Login()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
--- Lands/Lands/ViewModels/MainViewModel.cs
+++ Lands/Lands/ViewModels/MainViewModel.cs
@@ -2,8 +2,16 @@
 {
 
     public class MainViewModel
-	{
-		
-	}
-}
+    {
+        #region ViewModels
+        public LoginViewModel Login { get; set; }
+        #endregion
 
+        #region Constructors
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+        #endregion
+    }
+}
