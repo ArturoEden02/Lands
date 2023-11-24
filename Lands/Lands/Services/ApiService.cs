3c085042dd8761582642b93b72c6d9a613074d02
ArturoEden02 <arturo.aragonsanchez@hotmail.com>
2023-11-23 18:15:46 -0600

    Conectividad
    se inician a agregar los servicios para conectar a los servicios rest

---
+++ Lands/Lands/Services/ApiService.cs
@@ -0,0 +1,72 @@
+﻿namespace Lands.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Models;
+    using Newtonsoft.Json;
+    using Plugin.Connectivity;
+
+    public class ApiService
+    {
+        public async Task<Response> CheckConnection()
+        {
+            if (!CrossConnectivity.Current.IsConnected)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = "Please turn on your internet settings."
+                };
+            }
+
+            var isReachable = await CrossConnectivity.Current.IsRemoteReachable(
+                "google.com");
+
+            if (!isReachable)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = "Check you internet connection."
+                };
+            }
+            return new Response
+            {
+                IsSuccess = true,
+                Message = "Connection success."
+            };
+        }
+
+        public async Task<TokenResponse> GetToken(
+            string urlBase,
+            string userName,
+            string password)
+        {
+            try
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri(urlBase);
+                var response = await client.PostAsync(
+                    "Token",
+                    new StringContent(
+                        string.Format(
+                            "grant_type=password&username={0}&password={1}",
+                            userName,
+                            password),
+                        Encoding.UTF8, "application/x-www-form-unlencoded"));
+                var resultJSON = await response.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<TokenResponse>(
+                    resultJSON);
+                return result;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
